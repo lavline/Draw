19ab1937322c1fe01c1ff5c3012bb7248ad249ac
lavline <zhengyuliao5576@gmail.com>
2022-03-09 22:58:40 +0800

    change

--- cell.py
+++ cell.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 #this is a test
-f = open("C:/Users/zzt55/Desktop/cell_size.txt")
+f = open("C:/Users/vzt12/Desktop/cell_size.txt")
 lines = f.readlines()
 length = len(lines)
 data = np.zeros(length, dtype=int)
@@ -11,6 +11,7 @@ for line in lines:
     data[index] = formline[3]
     index += 1
 sum = 0
+sum1 = 0
 a1 = 0
 a2 = 0
 a3 = 0
@@ -20,21 +21,24 @@ for d in data:
         continue
     else:
         sum += 1
+        sum1 += d
     if d <= 10:
         a1 += 1
-    elif d <= 20:
+    elif d <= 100:
         a2 += 1
-    elif d <= 30:
+    elif d <= 1000:
         a3 += 1
     else:
         a4 += 1
 
-print("0-10: %d %.*f%%" % (a1, 5, a1 / sum * 100))
-print("10-20: %d %.*f%%" % (a2, 5, a2 / sum * 100))
-print("20-30: %d %.*f%%" % (a3, 5, a3 / sum * 100))
-print(">30: %d %.*f%%" % (a4, 5, a3 / sum * 100))
-print("max: %d" % (np.max(data)))
-print("std: %.*f" % (3, np.std(data)))
+print(">0:\t%d(%.*f%%)" % (sum, 3, sum / length * 100))
+print("1-10:\t%d(%.*f%%)" % (a1, 3, a1 / sum * 100))
+print("11-100:\t%d(%.*f%%)" % (a2, 3, a2 / sum * 100))
+print("101-1K:\t%d(%.*f%%)" % (a3, 3, a3 / sum * 100))
+print(">1K:\t%d(%.*f%%)" % (a4, 3, a4 / sum * 100))
+print("max:\t%d" % (np.max(data)))
+print("avg:\t%.*f" % (3, sum1 / sum))
+print("std:\t%.*f" % (3, np.std(data)))
 # data_1_len = 1000000
 # data_1 = data[0:data_1_len]
 
@@ -46,18 +50,19 @@ fig, ax = plt.subplots(constrained_layout=True)
 x_index = np.arange(length)
 
 # 设置左侧Y轴对应的figure
-ax.set_xlabel(f'{length} cells', size=20)
-ax.set_ylabel('Number', size=20)
+ax.set_xlabel(f'{length} cells', size=15)
+ax.set_ylabel('Number', size=15)
 
 # ax.bar(x, data_1, width)
-ax.scatter(x_index, data, s=5, alpha=0.5)
+ax.scatter(x_index, data, s=5, alpha=0.7)
 # ax.set_xticklabels(x_index)
 plt.xticks([])
 plt.yscale('log')
-plt.yticks([2, 4, 8, 10, 20, 30, 40, 50, 100, 200, 300],
-           ['2', '4', '8', '10', '20', '30', '40', '50', '100', '200', '300'], size=20)
+plt.yticks([1, 10, 100, 1000],
+          ['1', '10','100', '1K'], size=15)
 plt.grid(axis='y', linestyle='--')
-plt.text(1500000, 50, "0-10: %.*f%%\n"
+'''
+plt.text(1500000, 350, "0-10: %.*f%%\n"
                                   "10-20: %.*f%%\n"
                                   "20-30: %.*f%%\n"
                                   ">30: %.*f%%\n"
@@ -70,7 +75,9 @@ plt.text(1500000, 50, "0-10: %.*f%%\n"
                                                  sum,
                                                  np.max(data),
                                                  3, np.std(data)), size=18)
-fig.set_size_inches(21, 9)
+'''
+test_name = "33x33x33x33x5-acl1"
+plt.title(test_name, size=15)
+fig.set_size_inches(5, 3)
 plt.show()
-
-#fig.savefig('C:/Users/zzt55/Desktop/ACL_experiment/cell-33x257x65x4.png', dpi=300)
+fig.savefig('C:/Users/vzt12/Desktop/ACL_experiment/cell-'+test_name+'.png', dpi=300)
--- cycle_result.py
+++ cycle_result.py
@@ -1,11 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-f = open("C:/Users/zzt55/Desktop/match_cycle.txt")
+f = open("C:/Users/vzt12/Desktop/match_cycle.txt")
 lines = f.readlines()
 length = len(lines)
-data = np.zeros(length, dtype=int)
-check_rules = np.zeros(length, dtype=int)
+data = np.zeros(length, dtype=float)
+#check_rules = np.zeros(length, dtype=int)
 index = 0
 for line in lines:
     formline = line.split(' ')
@@ -19,52 +19,55 @@ a1 = 0
 a2 = 0
 a3 = 0
 for d in data:
-    if d <= 1000:
+    if d <= 200:
         a1 += 1
-    elif d <= 2000:
+    elif d <= 500:
         a2 += 1
     else:
         a3 += 1
 
-print("0-1k: %d %.*f%%" % (a1, 5, a1 / length * 100))
-print("1k-2k: %d %.*f%%" % (a2, 5, a2 / length * 100))
-print(">2k: %d %.*f%%" % (a3, 5, a3 / length * 100))
-print("max: %d" % (np.max(data)))
-print("min: %d" % (np.min(data)))
-print("average: %.*f" % (3, np.mean(data)))
-print("std: %.*f" % (3, np.std(data)))
+#print("0-200:\t%d\t%.*f%%" % (a1, 2, a1 / length * 100))
+#print("200-500:\t%d\t%.*f%%" % (a2, 2, a2 / length * 100))
+print(">500:\t%d(%.*f%%)" % (a3, 2, a3 / length * 100))
+print("max:\t%d" % (np.max(data)))
+print("min:\t%d" % (np.min(data)))
+print("avg:\t%.*f" % (3, np.mean(data)))
+print("std:\t%.*f" % (3, np.std(data)))
 
 
 fig, ax = plt.subplots(constrained_layout=True)
 
 x_index = np.arange(length)
 
-ax.set_xlabel(f'{length} messages', size=20)
-ax.set_ylabel('Cycle', size=20)
+#ax.set_xlabel(f'{length} messages', size=15)
+ax.set_ylabel('Cycle', size=15)
 
-ax.scatter(x_index, data, s=5, alpha=0.05)
+ax.scatter(x_index, data, s=5, alpha=0.4)
 #ax.plot(x_1, y_1, color='#FF3935')
 #ax.set_xticklabels(x_index)
 plt.xticks([])
-plt.yscale('log')
-plt.yticks([200, 300, 500, 1000, 2000, 3000, 10000, 50000], ['200', '300', '500', '1K', '2K', '3K', '10K', '50K'], size=20)
+#lt.yscale('log')
+#plt.yticks([200, 400, 800, 1600], ['2', '4', '8', '16'])
+plt.yticks(size=12)
 plt.grid(axis='y', linestyle='--')
-plt.text(1000000, 10000, "0-1k: %.*f%%\n"
-                                  "1k-2k: %.*f%%\n"
-                                  ">2k: %.*f%%\n"
+'''
+plt.text(1060000, 500, "0-200: %.*f%%\n"
+                                  "200-500: %.*f%%\n"
+                                  ">500: %.*f%%\n"
                                   "max: %d\n"
                                   "min: %d\n"
                                   "average: %.*f\n"
-                                  "std: %.*f" % (5, a1 / length * 100,
-                                                 5, a2 / length * 100,
-                                                 5, a3 / length * 100,
+                                  "std: %.*f" % (3, a1 / length * 100,
+                                                 3, a2 / length * 100,
+                                                 3, a3 / length * 100,
                                                  np.max(data),
                                                  np.min(data),
-                                                 3, np.mean(data),
+                                                 1, np.mean(data),
                                                  3, np.std(data)), size=18)
-test_name = "33x257x65x4-fw1"
-plt.title(test_name, size=20)
-fig.set_size_inches(21, 9)
+'''
+test_name = "65x65x33x33-ipc2-ARM"
+plt.title(test_name, size=15)
+fig.set_size_inches(5, 3)
 plt.show()
-#fig.savefig('C:/Users/zzt55/Desktop/ACL_experiment/match_cycle_'+test_name+'.png', dpi=300)
+fig.savefig('C:/Users/vzt12/Desktop/ACL_experiment/match_cycle_'+test_name+'.png', dpi=300)
 
